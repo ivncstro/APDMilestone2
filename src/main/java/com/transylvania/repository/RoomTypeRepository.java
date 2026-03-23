49ad12c8a8996ca23d7e630ff194f3f415f8e8d7
ivncstro <ivananthony167@gmail.com>
2026-03-22 23:19:38 -0400

    save function to save room type

--- src/main/java/com/transylvania/repository/RoomTypeRepository.java
+++ src/main/java/com/transylvania/repository/RoomTypeRepository.java
@@ -1,4 +1,27 @@
 package com.transylvania.repository;
 
+import com.transylvania.config.JpaUtil;
+import com.transylvania.model.RoomType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class RoomTypeRepository {
+
+    public void save(RoomType type) {
+        EntityManager em = JpaUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(type);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
 }
