ec07fb1e1047d16dbb819a1b8c4a0e93cb298e46
ivncstro <ivananthony167@gmail.com>
2026-03-22 23:17:18 -0400

    Factory Pattern 2 rooms of each

--- src/main/java/com/transylvania/model/RoomFactory.java
+++ src/main/java/com/transylvania/model/RoomFactory.java
@@ -1,11 +1,34 @@
-package com.transylvania.model;
+package com.transylvania.factory;
+
+import com.transylvania.model.Room;
+import com.transylvania.model.RoomType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomFactory {
-    public static RoomType createRoomType(String type, int capacity, double basePrice){
-        return new RoomType(type,capacity,basePrice);
 
-    }
-    public static Room createroom(int roomNumber,String status, RoomType type){
-        return new Room(roomNumber,status,type);
+    public static List<Room> createRooms() {
+        List<Room> rooms = new ArrayList<>();
+
+        RoomType single = new RoomType("Single", 2, 100);
+        RoomType doubleR = new RoomType("Double", 4, 180);
+        RoomType deluxe = new RoomType("Deluxe", 4, 250);
+        RoomType penthouse = new RoomType("Penthouse", 6, 500);
+
+        // create 2 of each type
+        rooms.add(new Room(101, "AVAILABLE", single));
+        rooms.add(new Room(102, "AVAILABLE", single));
+
+        rooms.add(new Room(201, "AVAILABLE", doubleR));
+        rooms.add(new Room(202, "AVAILABLE", doubleR));
+
+        rooms.add(new Room(301, "AVAILABLE", deluxe));
+        rooms.add(new Room(302, "AVAILABLE", deluxe));
+
+        rooms.add(new Room(401, "AVAILABLE", penthouse));
+        rooms.add(new Room(402, "AVAILABLE", penthouse));
+
+        return rooms;
     }
 }
